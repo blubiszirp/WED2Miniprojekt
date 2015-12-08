5d253eba1994f28bf37ad828c23a75bc4dcdb99d
blubiszirp <frank-jordi@hotmail.com>
2015-12-08 13:47:01 +0100

    add attribute canceled to guest

--- webapp/source/classes/models/guest.js
+++ webapp/source/classes/models/guest.js
@@ -5,11 +5,12 @@
 define(['app/services/uuidService'], function(UUIDService) {
     'use strict';
 
-    var Guest = function(name, contribution, comment,id) {
+    var Guest = function(name, contribution, comment, canceled, id) {
 
         this.id = id || UUIDService.getRandomUuid();
         this.name = name;
         this.contribution = contribution;
+        this.canceled = canceled;
         this.comment = comment;
     };
 
@@ -21,6 +22,7 @@ define(['app/services/uuidService'], function(UUIDService) {
             jsonData.name,
             jsonData.contribution,
             jsonData.comment,
+            jsonData.canceled,
             jsonData.id
         );
     };
